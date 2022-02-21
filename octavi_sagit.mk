5d0856dab08fb61b0a9417b06ba0e927c11969f5
foxlesbiao <addresseui@gmail.com>
2022-02-21 14:21:07 +0800

    sagit:octavi ify

--- octavi_sagit.mk
+++ octavi_sagit.mk
@@ -21,15 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common octavi stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
-# GMS
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
-$(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
-$(call inherit-product-if-exists, vendor/apps/GoogleCamera/config.mk)
-
-PRODUCT_NAME := aosp_sagit_gms
+PRODUCT_NAME := octavi_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
